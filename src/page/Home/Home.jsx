b23cba63d885e3df4e55f74d6d3666c52ff6abf0
Eltaj Mammadzada <eltacmemmedzade@gmail.com>
2024-04-27 14:27:25 +0400

    Portfolio section add

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,21 +1,22 @@
-import Navbar from '../../Layout/Navbar/Navbar'
+import Navbar from "../../Layout/Navbar/Navbar";
 import HeroImg from "../../assets/heroimg.svg";
 import One from "../../assets/1.svg";
 import Icon1 from "../../assets/icon1.svg";
 import Icon2 from "../../assets/icon2.svg";
 import Icon3 from "../../assets/icon3.svg";
 import Two from "../../assets/2.svg";
-import Icon4 from "../../assets/icon4.svg"
-import Icon5 from "../../assets/icon5.svg"
-import Icon6 from "../../assets/icon6.svg"
-import Icon7 from "../../assets/icon7.svg"
-import Icon8 from "../../assets/icon8.svg"
-import Icon9 from "../../assets/icon9.svg"
+import Icon4 from "../../assets/icon4.svg";
+import Icon5 from "../../assets/icon5.svg";
+import Icon6 from "../../assets/icon6.svg";
+import Icon7 from "../../assets/icon7.svg";
+import Icon8 from "../../assets/icon8.svg";
+import Icon9 from "../../assets/icon9.svg";
+import Three from "../../assets/3.svg";
 
 export default function Home() {
   return (
     <>
-          <section className="bg-herobg">
+      <section className="bg-herobg">
         <Navbar />
         <div className="container flex flex-col justify-center items-center pt-[60px]">
           <span className="text-white font-dm-sans font-medium text-lg uppercase tracking-[2px] pb-[26px]">
@@ -108,25 +109,101 @@ export default function Home() {
             </h3>
           </div>
           <div className="col-span-5">
-            <span className="text-lg font-dm-sans font-normal tracking-[2px] uppercase text-Primary_1">reprehendunt</span>
-            <h3 className="pt-6 text-[#333] lg:text-3xl text-2xl font-dm-sans font-bold lg:leading-[64px] leading-[55px]">Quo luptatum evertitur</h3>
+            <span className="text-lg font-dm-sans font-normal tracking-[2px] uppercase text-Primary_1">
+              reprehendunt
+            </span>
+            <h3 className="pt-6 text-[#333] lg:text-3xl text-2xl font-dm-sans font-bold lg:leading-[64px] leading-[55px]">
+              Quo luptatum evertitur
+            </h3>
             <p className="pt-7 text-lg font-dm-sans font-normal text-[#333]">
               Altera integre suavitate per an, alienum phaedrum te sea. Ex sea
               causae dolores, nam et doming dicunt feugait. Ea mel scripta
               aperiri postulant.
             </p>
             <ul className="grid grid-cols-2 gap-8 pt-14 pb-5">
-              <li className="flex items-center gap-3"><img src={Icon4} alt="" /><span>Lorem</span></li>
-              <li className="flex items-center gap-3"><img src={Icon5} alt="" /><span>Lorem</span></li>
-              <li className="flex items-center gap-3"><img src={Icon6} alt="" /><span>Lorem</span></li>
-              <li className="flex items-center gap-3"><img src={Icon7} alt="" /><span>Lorem</span></li>
-              <li className="flex items-center gap-3"><img src={Icon8} alt="" /><span>Lorem</span></li>
-              <li className="flex items-center gap-3"><img src={Icon9} alt="" /><span>Lorem</span></li>
+              <li className="flex items-center gap-3">
+                <img src={Icon4} alt="" />
+                <span>Lorem</span>
+              </li>
+              <li className="flex items-center gap-3">
+                <img src={Icon5} alt="" />
+                <span>Lorem</span>
+              </li>
+              <li className="flex items-center gap-3">
+                <img src={Icon6} alt="" />
+                <span>Lorem</span>
+              </li>
+              <li className="flex items-center gap-3">
+                <img src={Icon7} alt="" />
+                <span>Lorem</span>
+              </li>
+              <li className="flex items-center gap-3">
+                <img src={Icon8} alt="" />
+                <span>Lorem</span>
+              </li>
+              <li className="flex items-center gap-3">
+                <img src={Icon9} alt="" />
+                <span>Lorem</span>
+              </li>
             </ul>
-            <button className="text-[#F36B7F] border-[#F36B7F] border-solid border py-3 px-7 rounded-3xl text-base font-dm-sans font-bold">Read More</button>
+            <button className="text-[#F36B7F] border-[#F36B7F] border-solid border py-3 px-7 rounded-3xl text-base font-dm-sans font-bold">
+              Read More
+            </button>
           </div>
         </div>
       </section>
+
+      <section className="py-12 container">
+        <div className="gap-6 grid lg:grid-cols-2 grid-cols-1">
+          <div>
+            <span className="text-lg font-dm-sans font-normal uppercase text-Primary_1 tracking-[2px] pb-6">animal</span>
+            <h3 className="pb-12 lg:text-3xl lg:leading-[64px] text-2xl leading-[55px] text-[#333] font-dm-sans font-bold">Quidam officiis similique </h3>
+            <div className="bg-[#FFF3D4] rounded-lg">
+              <img src={Three} alt="" className="w-full" />
+              <div className="py-10 px-8">
+                <h3 className="pb-2 text-xl font-dm-sans font-bold leading-8 text-[#333]">Efficiendi nihil</h3>
+                <span className="text-base font-normal font-dm-sans text-Text_Black_50%">Exerci</span>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-6 pt-[70px]">
+              <div className="col-span-5">
+                <p className="pb-4 text-base font-dm-sans font-normal text-Text_Black_50%">
+                  Dicat eripuit accumsan ad est. Facer facilisi ius ei, ea has
+                  cetero argumentum, vel at fugit temporibus.
+                </p>
+                <p className="text-base font-dm-sans font-normal text-Text_Black_50%">
+                  Deseruisse definitionem his et, an has veri integre
+                  abhorreant, nam alii epicurei et. Ei ferri aperiri pri. Nemore
+                  facilisi mediocrem at duo, zril ancillae ea sit.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="gap-6 flex flex-col">
+
+          <div className="bg-[#EDF3FF] rounded-lg">
+              <img src={Three} alt="" className="w-full" />
+              <div className="py-10 px-8">
+                <h3 className="pb-2 text-xl font-dm-sans font-bold leading-8 text-[#333]">Tollit indoctum</h3>
+                <span className="text-base font-normal font-dm-sans text-Text_Black_50%">Contentiones</span>
+              </div>
+            </div>
+
+            <div className="bg-[#D0F2E8] rounded-lg">
+              <img src={Three} alt="" className="w-full" />
+              <div className="py-10 px-8">
+                <h3 className="pb-2 text-xl font-dm-sans font-bold leading-8 text-[#333]">Tantas platonem</h3>
+                <span className="text-base font-normal font-dm-sans text-Text_Black_50%">Dissentiet</span>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <div className="flex justify-center w-full pt-[64px]">
+        <button className="border border-solid border-[#F36B7F] text-[#F36B7F] py-3 px-10 rounded-3xl font-dm-sans font-bold text-base">All work</button>
+        </div>
+      </section>
     </>
-  )
+  );
 }
